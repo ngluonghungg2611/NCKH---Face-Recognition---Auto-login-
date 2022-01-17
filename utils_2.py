4ec9ba935208e5681d6890c1e9f2506cb733b255
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2022-01-17 23:17:17 +0700

    utils

--- utils_2.py
+++ utils_2.py
@@ -12,8 +12,6 @@ def _crop_face_224(img):
     faces = face_cascade.detectMultiScale(img_gray, scaleFactor = 1.5, minNeighbors = 5)
     for (x,y,w,h) in faces:
         img_face = img[y: y + h, x: x + w]
-    # img_face = cv2.resize(img_face, (224,224))
-    # cv2.imwrite(img_path + img_name, img_face)
     return img_face
 def _crop_face_224(img):
     face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
